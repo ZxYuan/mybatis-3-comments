af8ad9745df588903dab4784d023cdbd280ac6b1
ZxYuan <lvp0526@gmail.com>
2015-07-31 15:34:13 +0900

    Add comments to LruCache.java

--- src/main/java/org/apache/ibatis/cache/decorators/LruCache.java
+++ src/main/java/org/apache/ibatis/cache/decorators/LruCache.java
@@ -26,10 +26,10 @@ import org.apache.ibatis.cache.Cache;
  *
  * @author Clinton Begin
  */
-public class LruCache implements Cache {
+public class LruCache implements Cache { // LRU最近最少使用算法的Cache装饰器
 
-  private final Cache delegate;
-  private Map<Object, Object> keyMap;
+  private final Cache delegate; // 被装饰的Cache
+  private Map<Object, Object> keyMap; // <key, key> 存缓存项的访问顺序
   private Object eldestKey;
 
   public LruCache(Cache delegate) {
@@ -47,15 +47,15 @@ public class LruCache implements Cache {
     return delegate.getSize();
   }
 
-  public void setSize(final int size) {
-    keyMap = new LinkedHashMap<Object, Object>(size, .75F, true) {
+  public void setSize(final int size) { // 初始化map
+    keyMap = new LinkedHashMap<Object, Object>(size, .75F, true) { // 继承LinkedHashMap的匿名类, true指定access-order
       private static final long serialVersionUID = 4267176411845948333L;
 
-      @Override
-      protected boolean removeEldestEntry(Map.Entry<Object, Object> eldest) {
-        boolean tooBig = size() > size;
+      @Override // 默认返回false；在每次addEntry后判断这个方法是否为true，若为true则在map中删除eldest
+      protected boolean removeEldestEntry(Map.Entry<Object, Object> eldest) { // override 移除最老项
+        boolean tooBig = size() > size; // linkedMapHash的size大于缓存的指定size
         if (tooBig) {
-          eldestKey = eldest.getKey();
+          eldestKey = eldest.getKey(); // 保存目前最老项的key，之后该项在linkedHashMap中立即会被删除
         }
         return tooBig;
       }
@@ -63,14 +63,14 @@ public class LruCache implements Cache {
   }
 
   @Override
-  public void putObject(Object key, Object value) {
+  public void putObject(Object key, Object value) { // 加入新的缓存项
     delegate.putObject(key, value);
-    cycleKeyList(key);
+    cycleKeyList(key); // 每次加入时调用一次
   }
 
   @Override
   public Object getObject(Object key) {
-    keyMap.get(key); //touch
+    keyMap.get(key); //touch // 改变access-order
     return delegate.getObject(key);
   }
 
@@ -90,11 +90,11 @@ public class LruCache implements Cache {
     return null;
   }
 
-  private void cycleKeyList(Object key) {
-    keyMap.put(key, key);
-    if (eldestKey != null) {
-      delegate.removeObject(eldestKey);
-      eldestKey = null;
+  private void cycleKeyList(Object key) { // 每次加入新的缓存项后时被调用
+    keyMap.put(key, key); // 往linkedHashMap里加入这个新缓存项的key
+    if (eldestKey != null) { // 最老项非空，意味着有在linkedHashMap中被删了但在缓存中还未被删的具有同样key的项
+      delegate.removeObject(eldestKey); // 从缓存中删除最老的缓存项
+      eldestKey = null; // 没有剩余没被处理的已废弃缓存项了
     }
   }
 
