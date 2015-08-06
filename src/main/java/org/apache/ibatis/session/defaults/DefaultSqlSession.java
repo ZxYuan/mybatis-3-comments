20f57864a2ff807de8bbcad25120c828a874e08f
ZxYuan <lvp0526@gmail.com>
2015-08-07 00:43:04 +0900

    Update DefaultSqlSession.java

--- src/main/java/org/apache/ibatis/session/defaults/DefaultSqlSession.java
+++ src/main/java/org/apache/ibatis/session/defaults/DefaultSqlSession.java
@@ -46,14 +46,14 @@ import org.apache.ibatis.session.SqlSession;
  *
  * @author Clinton Begin
  */
-public class DefaultSqlSession implements SqlSession {
+public class DefaultSqlSession implements SqlSession { // 默认sqlSession
 
-  private Configuration configuration;
-  private Executor executor;
+  private Configuration configuration; // mybatis的configuration
+  private Executor executor; // 用于真正执行crud等操作
 
-  private boolean autoCommit;
-  private boolean dirty;
-  private List<Cursor<?>> cursorList;
+  private boolean autoCommit; // 标识是否自动提交
+  private boolean dirty; // hama
+  private List<Cursor<?>> cursorList; // 游标list
 
   public DefaultSqlSession(Configuration configuration, Executor executor, boolean autoCommit) {
     this.configuration = configuration;
@@ -72,11 +72,11 @@ public class DefaultSqlSession implements SqlSession {
   }
 
   @Override
-  public <T> T selectOne(String statement, Object parameter) {
+  public <T> T selectOne(String statement, Object parameter) { // 返回一条记录
     // Popular vote was to return null on 0 results and throw exception on too many.
-    List<T> list = this.<T>selectList(statement, parameter);
+    List<T> list = this.<T>selectList(statement, parameter); // 先取出一个list
     if (list.size() == 1) {
-      return list.get(0);
+      return list.get(0); // 再返回第一个元素
     } else if (list.size() > 1) {
       throw new TooManyResultsException("Expected one result (or null) to be returned by selectOne(), but found: " + list.size());
     } else {
@@ -95,9 +95,9 @@ public class DefaultSqlSession implements SqlSession {
   }
 
   @Override
-  public <K, V> Map<K, V> selectMap(String statement, Object parameter, String mapKey, RowBounds rowBounds) {
-    final List<? extends V> list = selectList(statement, parameter, rowBounds);
-    final DefaultMapResultHandler<K, V> mapResultHandler = new DefaultMapResultHandler<K, V>(mapKey,
+  public <K, V> Map<K, V> selectMap(String statement, Object parameter, String mapKey, RowBounds rowBounds) { // hama
+    final List<? extends V> list = selectList(statement, parameter, rowBounds); // 取出一个list
+    final DefaultMapResultHandler<K, V> mapResultHandler = new DefaultMapResultHandler<K, V>(mapKey, // hama
         configuration.getObjectFactory(), configuration.getObjectWrapperFactory(), configuration.getReflectorFactory());
     final DefaultResultContext<V> context = new DefaultResultContext<V>();
     for (V o : list) {
@@ -118,16 +118,16 @@ public class DefaultSqlSession implements SqlSession {
   }
 
   @Override
-  public <T> Cursor<T> selectCursor(String statement, Object parameter, RowBounds rowBounds) {
+  public <T> Cursor<T> selectCursor(String statement, Object parameter, RowBounds rowBounds) { // 获得游标
     try {
-      MappedStatement ms = configuration.getMappedStatement(statement);
-      Cursor<T> cursor = executor.queryCursor(ms, wrapCollection(parameter), rowBounds);
-      registerCursor(cursor);
+      MappedStatement ms = configuration.getMappedStatement(statement); // 获得MappedStatment
+      Cursor<T> cursor = executor.queryCursor(ms, wrapCollection(parameter), rowBounds); // 用executor执行查询
+      registerCursor(cursor); // 注册游标 hama
       return cursor;
     } catch (Exception e) {
       throw ExceptionFactory.wrapException("Error querying database.  Cause: " + e, e);
     } finally {
-      ErrorContext.instance().reset();
+      ErrorContext.instance().reset(); // hama
     }
   }
 
@@ -142,14 +142,14 @@ public class DefaultSqlSession implements SqlSession {
   }
 
   @Override
-  public <E> List<E> selectList(String statement, Object parameter, RowBounds rowBounds) {
+  public <E> List<E> selectList(String statement, Object parameter, RowBounds rowBounds) { // 取出list
     try {
-      MappedStatement ms = configuration.getMappedStatement(statement);
-      return executor.query(ms, wrapCollection(parameter), rowBounds, Executor.NO_RESULT_HANDLER);
+      MappedStatement ms = configuration.getMappedStatement(statement); // 获得MappedStatement
+      return executor.query(ms, wrapCollection(parameter), rowBounds, Executor.NO_RESULT_HANDLER); // 用executor查询
     } catch (Exception e) {
       throw ExceptionFactory.wrapException("Error querying database.  Cause: " + e, e);
     } finally {
-      ErrorContext.instance().reset();
+      ErrorContext.instance().reset(); // hama
     }
   }
 
@@ -164,14 +164,14 @@ public class DefaultSqlSession implements SqlSession {
   }
 
   @Override
-  public void select(String statement, Object parameter, RowBounds rowBounds, ResultHandler handler) {
+  public void select(String statement, Object parameter, RowBounds rowBounds, ResultHandler handler) { // 有啥用 hama
     try {
-      MappedStatement ms = configuration.getMappedStatement(statement);
-      executor.query(ms, wrapCollection(parameter), rowBounds, handler);
+      MappedStatement ms = configuration.getMappedStatement(statement); // 取出MappedStatement
+      executor.query(ms, wrapCollection(parameter), rowBounds, handler); // 用executor执行查询，然后并没有返回
     } catch (Exception e) {
       throw ExceptionFactory.wrapException("Error querying database.  Cause: " + e, e);
     } finally {
-      ErrorContext.instance().reset();
+      ErrorContext.instance().reset(); // hama
     }
   }
 
@@ -191,20 +191,20 @@ public class DefaultSqlSession implements SqlSession {
   }
 
   @Override
-  public int update(String statement, Object parameter) {
+  public int update(String statement, Object parameter) { // 更新
     try {
-      dirty = true;
-      MappedStatement ms = configuration.getMappedStatement(statement);
-      return executor.update(ms, wrapCollection(parameter));
+      dirty = true; // 标记dirty
+      MappedStatement ms = configuration.getMappedStatement(statement); // 获得MappedStatement
+      return executor.update(ms, wrapCollection(parameter)); // 用executor执行update
     } catch (Exception e) {
       throw ExceptionFactory.wrapException("Error updating database.  Cause: " + e, e);
     } finally {
-      ErrorContext.instance().reset();
+      ErrorContext.instance().reset(); // hama
     }
   }
 
   @Override
-  public int delete(String statement) {
+  public int delete(String statement) { // 使用update
     return update(statement, null);
   }
 
@@ -221,12 +221,12 @@ public class DefaultSqlSession implements SqlSession {
   @Override
   public void commit(boolean force) {
     try {
-      executor.commit(isCommitOrRollbackRequired(force));
-      dirty = false;
+      executor.commit(isCommitOrRollbackRequired(force)); // 使用executor执行提交
+      dirty = false; // 提交后没有脏数据
     } catch (Exception e) {
       throw ExceptionFactory.wrapException("Error committing transaction.  Cause: " + e, e);
     } finally {
-      ErrorContext.instance().reset();
+      ErrorContext.instance().reset(); // hama
     }
   }
 
@@ -236,19 +236,19 @@ public class DefaultSqlSession implements SqlSession {
   }
 
   @Override
-  public void rollback(boolean force) {
+  public void rollback(boolean force) { // 回滚
     try {
-      executor.rollback(isCommitOrRollbackRequired(force));
-      dirty = false;
+      executor.rollback(isCommitOrRollbackRequired(force)); // 用executor执行回滚
+      dirty = false; // 回滚后没有脏数据
     } catch (Exception e) {
       throw ExceptionFactory.wrapException("Error rolling back transaction.  Cause: " + e, e);
     } finally {
-      ErrorContext.instance().reset();
+      ErrorContext.instance().reset(); // hama
     }
   }
 
   @Override
-  public List<BatchResult> flushStatements() {
+  public List<BatchResult> flushStatements() { // hama
     try {
       return executor.flushStatements();
     } catch (Exception e) {
@@ -259,41 +259,41 @@ public class DefaultSqlSession implements SqlSession {
   }
 
   @Override
-  public void close() {
+  public void close() { // 关闭session
     try {
-      executor.close(isCommitOrRollbackRequired(false));
-      closeCursors();
-      dirty = false;
+      executor.close(isCommitOrRollbackRequired(false)); // 用executor执行close
+      closeCursors(); // hama
+      dirty = false; // close后没有脏数据
     } finally {
       ErrorContext.instance().reset();
     }
   }
 
   private void closeCursors() {
-    if (cursorList != null && cursorList.size() != 0) {
-      for (Cursor<?> cursor : cursorList) {
+    if (cursorList != null && cursorList.size() != 0) { // 还有游标存在
+      for (Cursor<?> cursor : cursorList) { // 遍历关闭所有游标
         try {
           cursor.close();
         } catch (IOException e) {
           throw ExceptionFactory.wrapException("Error closing cursor.  Cause: " + e, e);
         }
       }
-      cursorList.clear();
+      cursorList.clear(); // 清空游标list
     }
   }
 
   @Override
-  public Configuration getConfiguration() {
+  public Configuration getConfiguration() { // 获得mybatis的Configuration
     return configuration;
   }
 
   @Override
-  public <T> T getMapper(Class<T> type) {
+  public <T> T getMapper(Class<T> type) { // 获得mapper，是啥 hama
     return configuration.<T>getMapper(type, this);
   }
 
   @Override
-  public Connection getConnection() {
+  public Connection getConnection() { // 从executor获得事务的连接
     try {
       return executor.getTransaction().getConnection();
     } catch (SQLException e) {
@@ -302,35 +302,35 @@ public class DefaultSqlSession implements SqlSession {
   }
 
   @Override
-  public void clearCache() {
-    executor.clearLocalCache();
+  public void clearCache() { // 清空缓存
+    executor.clearLocalCache(); // 但是究竟做了啥 hama
   }
 
-  private <T> void registerCursor(Cursor<T> cursor) {
+  private <T> void registerCursor(Cursor<T> cursor) { // 注册游标
     if (cursorList == null) {
       cursorList = new ArrayList<Cursor<?>>();
     }
-    cursorList.add(cursor);
+    cursorList.add(cursor); // 游标加入list
   }
 
   private boolean isCommitOrRollbackRequired(boolean force) {
-    return (!autoCommit && dirty) || force;
+    return (!autoCommit && dirty) || force; // 非自动提交且有脏数据 或 强制提交开关开启
   }
 
-  private Object wrapCollection(final Object object) {
-    if (object instanceof Collection) {
+  private Object wrapCollection(final Object object) { // 包装，用于送入update方法的参数
+    if (object instanceof Collection) { // 是个Collection
       StrictMap<Object> map = new StrictMap<Object>();
-      map.put("collection", object);
-      if (object instanceof List) {
-        map.put("list", object);
+      map.put("collection", object); // <"collenction", object>
+      if (object instanceof List) { // 是个Collection且是个List
+        map.put("list", object); // <"list", object>
       }
       return map;
-    } else if (object != null && object.getClass().isArray()) {
+    } else if (object != null && object.getClass().isArray()) { // 是个非null的数组
       StrictMap<Object> map = new StrictMap<Object>();
-      map.put("array", object);
+      map.put("array", object); // <"array", object>
       return map;
     }
-    return object;
+    return object; // 其他情况不包装，直接返回object
   }
 
   public static class StrictMap<V> extends HashMap<String, V> {
